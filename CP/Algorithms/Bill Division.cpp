75a056faf255e95fbcd1e7467befed70fa51724c
ShresthaBanerjee01 <80540851+ShresthaBanerjee01@users.noreply.github.com>
2021-08-04 14:30:30 +0530

    Create Bill Division.cpp

---
+++ CP/Algorithms/Bill Division.cpp
@@ -0,0 +1,181 @@
+/*
+Two friends Anna and Brian, are deciding how to split the bill at a dinner. Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+
+For example, assume the bill has the following prices: . Anna declines to eat item  which costs . If Brian calculates the bill correctly, Anna will pay . If he includes the cost of , he will calculate . In the second case, he should refund  to Anna.
+
+Function Description
+
+Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
+
+bonAppetit has the following parameter(s):
+
+bill: an array of integers representing the cost of each item ordered
+k: an integer representing the zero-based index of the item Anna doesn't eat
+b: the amount of money that Anna contributed to the bill
+Input Format
+
+The first line contains two space-separated integers  and , the number of items ordered and the -based index of the item that Anna did not eat.
+The second line contains  space-separated integers  where .
+The third line contains an integer, , the amount of money that Brian charged Anna for her share of the bill.
+
+Constraints
+
+The amount of money due Anna will always be an integer
+Output Format
+
+If Brian did not overcharge Anna, print Bon Appetit on a new line; otherwise, print the difference (i.e., ) that Brian must refund to Anna. This will always be an integer.
+
+Sample Input 0
+
+4 1
+3 10 2 9
+12
+Sample Output 0
+
+5
+Explanation 0
+Anna didn't eat item , but she shared the rest of the items with Brian. The total cost of the shared items is  and, split in half, the cost per person is . Brian charged her  for her portion of the bill. We print the amount Anna was overcharged, , on a new line.
+
+Sample Input 1
+
+4 1
+3 10 2 9
+7
+Sample Output 1
+
+Bon Appetit
+Explanation 1
+Anna didn't eat item , but she shared the rest of the items with Brian. The total cost of the shared items is  and, split in half, the cost per person is . Because , we print Bon Appetit on a new line.
+
+
+
+CODE*******************************************************************************************************
+
+
+
+
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+void bonAppetit(vector<int> bill, int k, int b) {
+    int sum , a ,c,d;
+    sum =0;
+    for(int i=0;i<bill.size();i++)
+    {
+        sum = sum + bill[i];
+    }
+    a = sum - bill[k];
+    c = a/2;
+    d = b - c;
+    if(d==0)
+    {
+        cout << "Bon Appetit" << endl;
+    }
+    else {
+    cout << d << endl;
+    }
+}
+
+int main()
+{
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string bill_temp_temp;
+    getline(cin, bill_temp_temp);
+
+    vector<string> bill_temp = split(rtrim(bill_temp_temp));
+
+    vector<int> bill(n);
+
+    for (int i = 0; i < n; i++) {
+        int bill_item = stoi(bill_temp[i]);
+
+        bill[i] = bill_item;
+    }
+
+    string b_temp;
+    getline(cin, b_temp);
+
+    int b = stoi(ltrim(rtrim(b_temp)));
+
+    bonAppetit(bill, k, b);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
