dad550cb2704d66d98416ee20fe0229917287452
ShresthaBanerjee01 <80540851+ShresthaBanerjee01@users.noreply.github.com>
2021-08-07 02:39:02 +0530

    Update Number line jumps.cpp

--- CP/Algorithms/Number line jumps.cpp
+++ CP/Algorithms/Number line jumps.cpp
@@ -19,17 +19,17 @@ vector<string> split(const string &);
 
 string kangaroo(int x1, int v1, int x2, int v2) 
 {
-    if(x2>x1 && v2>v1)
+    if(v1>v2)
     {
-        return "NO";
-    }
-    else if ((x2-x1)%(v1-v2)==0)
+     if ((x2-x1)%(v1-v2)==0)
     {
         return "YES";
     }
     else {
     return "NO";
     }
+    }
+    return "NO";
 }
 
 int main()
