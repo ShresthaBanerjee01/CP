8caa2b78fa0558b01d3f8537965c63172c8901aa
ShresthaBanerjee01 <80540851+ShresthaBanerjee01@users.noreply.github.com>
2021-08-27 16:26:22 +0530

    Update Finding factors.cpp

--- CodeChef/DSA Learning Series/Easy Problems to Get Started With/Finding factors.cpp
+++ CodeChef/DSA Learning Series/Easy Problems to Get Started With/Finding factors.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int n , c =0;
+	cin >> n;
+	for(int i=1;i<=n;i++)
+	{
+	   if(n%i==0)
+	   {
+	       c++;
+	   }
+	}
+	cout << c << endl;
+	for(int i=1;i<=n;i++)
+	{
+	   if(n%i==0)
+	   {
+	        cout << i <<" "; 
+	   }
+	}
+	return 0;
+}
