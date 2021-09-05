1c9364e0e94b53a44879261fe2d7eaab378a17c4
ShresthaBanerjee01 <80540851+ShresthaBanerjee01@users.noreply.github.com>
2021-09-05 12:24:29 +0530

    Create Drawing Book.cpp

---
+++ CP/Algorithms/Drawing Book.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n1, int p1) {
+    float front , end;
+    float p,n;
+    p=p1;
+    n=n1;
+    front=0;
+    end=0;
+    if(n1%2==1)
+    {
+        if(p1%2==1)
+        {
+            front = (p-1)/2;
+            end = (n-p)/2;
+        }
+        else if(p1%2==0)
+        {
+            front = ceil((p-1)/2);
+            end = floor((n-p)/2);
+        }
+    }
+    else if(n1%2==0)
+    {
+        front =0;
+        end =0;
+        if(p1%2==1)
+        {
+            front = ceil((p-1)/2);
+            end = ceil((n-p)/2);
+        }
+        else if(p1%2==0)
+        {
+            front = ceil((p-1)/2);
+            end = ((n-p)/2);
+        }
+    }
+    int f;
+    f = fmin(front , end);
+    return f;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
